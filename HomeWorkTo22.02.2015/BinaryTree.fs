eb27916d8cd68d2101199ecd098a282c2cb50144
minonastya <minovskayanastya@gmail.com>
2015-02-22 03:38:21 +0300

    Peano and BST

---
+++ HomeWorkTo22.02.2015/BinaryTree.fs
@@ -0,0 +1,50 @@
+﻿type Tree = Nil | T of Tree * int * Tree
+
+let rec add v tr = 
+  match tr with
+  |Nil -> T(Nil, v, Nil)
+  |T(left, x, right) -> if (v > x) then T(left, x, (add v right))
+                        else if (v <x) then T((add v left), x, right)
+                        else T(left, x, right)
+
+let rNumb tr = //значение в правом узле
+  match tr with
+  |Nil -> 0
+  |T(left,x,right) ->x
+
+let rec del v tr =
+  match tr with
+  |Nil -> Nil
+  |T(left, x, right) -> if (v > x) then T(left, x, (del v right))
+                        else if (v < x) then T((del v left), x, right)
+                        else match right with
+                             |Nil -> left
+                             |right -> if (left = Nil) then right
+                                       else T(left, rNumb right, (del (rNumb right) right))
+
+let rec LCR tr =
+  match tr with
+  |Nil -> printf ""
+  |T(left, x, right) -> LCR left
+                        printf"%d" x
+                        LCR right
+               
+let rec LRC tr =
+  match tr with
+  |Nil -> printf ""
+  |T(left, x, right) -> LRC left
+                        LRC right
+                        printf"%d" x
+                       
+
+let rec CLR tr =
+  match tr with
+  |Nil -> printf ""
+  |T(left, x, right) -> printf"%d" x
+                        CLR left
+                        CLR right
+                        
+[<EntryPoint>]
+let main argv = 
+    printf "%A\n" (CLR (del 4(add 5(add 7 (add 6(add 3 (add 4 Nil)))))))
+    0 
---
+++ HomeWorkTo22.02.2015/Peano.fs
@@ -0,0 +1,56 @@
+﻿type Peano = Zero|S of Peano
+
+let suc(p:Peano) = S p 
+
+let minus1(p:Peano) = 
+  match p with
+  |Zero -> Zero
+  |S p -> p
+
+let rec plus a b =
+  match a, b with
+  |Zero, Zero -> Zero
+  |S a, Zero -> S a
+  |Zero, S b -> S b
+  |S a, S b -> S( S(plus a b))
+
+let rec minus a b =
+  match a, b with
+  |Zero, Zero -> Zero
+  |S a, Zero -> S a
+  |Zero, S b -> S b
+  |S a, S b -> minus a b
+
+let rec toInt a =
+  match a with
+  |Zero -> 0
+  |S a -> 1 + toInt a  
+
+let intToStr a =
+  a.ToString()
+
+let printPeano a = a |> toInt
+                     |> intToStr
+                     |> printf "%s\n"
+                     
+let rec multi a b =
+  match a with
+  |Zero -> Zero
+  |S a -> plus(multi a b) b
+  
+let rec exp a b = 
+  match b with
+  |Zero -> (S(Zero))
+  |S b -> multi a (exp a b)
+
+
+[<EntryPoint>]
+let main args = 
+    printf "%A\n" (suc(Zero))
+    printf "%A\n" (minus1 (S(S(S(S(Zero))))))
+    printf "%A\n" (plus (S(S(Zero))) (S(S(S(Zero)))))
+    printf "%A\n" (minus (S(S(S(Zero)))) (S(S(S(Zero)))))
+    printf "%A\n" (printPeano (S(S(S(S(S(S(S(Zero)))))))))
+    printf "%A\n" (multi (S(S(S(Zero)))) (S(S(Zero))))
+    printf "%A\n" (exp (S(S(Zero))) (S(S(S(Zero)))))
+    0 
