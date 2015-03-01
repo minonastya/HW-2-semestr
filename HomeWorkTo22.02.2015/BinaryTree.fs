333fe16643bccc92d0a03949ec0dd2e7e59812b6
minonastya <minovskayanastya@gmail.com>
2015-03-01 15:29:04 +0300

    BinaryTree
    Correcting HW

--- HomeWorkTo22.02.2015/BinaryTree.fs
+++ HomeWorkTo22.02.2015/BinaryTree.fs
@@ -8,7 +8,20 @@ let rec add v tr =
                         else if (v <x) then T((add v left), x, right)
                         else T(left, x, right)
 
-let rNumb tr = //значение в правом узле
+let rec rInLeft tr = //значение в самом правом узле левого поддерева
+  match tr with
+  |Nil -> 0
+  |T(left, x, Nil)-> x
+  |T(left, x, right) -> (rInLeft right)
+
+let rec lInRight tr = //значение в самом левом узле правого поддерева
+  match tr with
+  |Nil -> 0
+  |T(Nil, x, right)-> x
+  |T(left, x, right) -> (lInRight left)
+  
+
+let Numb tr = //значение в узле
   match tr with
   |Nil -> 0
   |T(left,x,right) ->x
@@ -18,10 +31,12 @@ let rec del v tr =
   |Nil -> Nil
   |T(left, x, right) -> if (v > x) then T(left, x, (del v right))
                         else if (v < x) then T((del v left), x, right)
-                        else match right with
-                             |Nil -> left
-                             |right -> if (left = Nil) then right
-                                       else T(left, rNumb right, (del (rNumb right) right))
+                        else match left, right with
+                             |Nil, Nil -> Nil
+                             |left, Nil -> left
+                             |Nil, right -> right
+                             |T(l, x, r), right -> if r = Nil then T(left, lInRight right, del (lInRight right) right)
+                                                   else T(del (rInLeft left) left, rInLeft left, right )
 
 let rec LCR tr =
   match tr with
@@ -47,27 +62,40 @@ let rec CLR tr =
                         
 [<EntryPoint>]
 let main argv = 
-    let tree = (add 3( add 10(add 1( add 7(add 4 Nil)))))
-    LRC tree
+    let tree = (add 4 Nil)
+    printf "%A" tree
     printf"\n"
-    CLR tree
+    printf "Insert 7: "
+    let tree = (add 7 tree)
+    printf "%A" tree
+    printf"\n"
+    printf "Insert 1: "
+    let tree = (add 1 tree)
+    printf "%A" tree
+    printf"\n"
+    printf "Insert 5: "
+    let tree = (add 5 tree)
+    printf "%A" tree
+    printf"\n"
+    printf "Insert 2: "
+    let tree = (add 2 tree)
+    printf "%A" tree
+    printf"\n"
+    printf "Insert 10: "
+    let tree = (add 10 tree)
+    printf "%A" tree
+    printf"\n"
+    printf "Delete 7: "
+    let tree = (del 7 tree)
+    printf "%A" tree
+    printf"\n"
+    printf "Delete 4: "
+    let tree = (del 4 tree)
+    printf "%A" tree
     printf"\n"
     LCR tree
     printf"\n"
-    printf"\n"
-    let tree = (add 8(add 20(add 2(add 16(add 12 tree)))))
     LRC tree
     printf"\n"
     CLR tree
-    printf"\n"
-    LCR tree
-    printf"\n"
-    printf"\n"
-    let tree = (del 0(del 4(del 16(del 5 tree))))
-    LRC tree
-    printf"\n"
-    CLR tree
-    printf"\n"
-    LCR tree
-    printf"\n"
     0 
