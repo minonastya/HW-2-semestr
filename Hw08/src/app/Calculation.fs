f71c1ea67eb231f8e47e8a3e758cef137f26d6b1
minonastya <minovskayanastya@gmail.com>
2015-04-26 03:39:02 +0400

    Added 42 task

---
+++ Hw08/src/app/Calculation.fs
@@ -0,0 +1,160 @@
+﻿module Calculation
+
+open System
+
+type Stack<'A> = 
+  interface
+    abstract push : 'A -> unit
+    abstract pop  : Option<'A>
+    abstract isEmpty : bool
+    abstract top  : Option<'A>
+    abstract printf : unit
+    abstract size : int
+    abstract rev : unit
+  end
+
+type ListStack<'A> () = 
+  class
+    let mutable list = []
+    interface Stack<'A> with 
+      member s.push a = list <- a ::list
+      member s.isEmpty = list.IsEmpty 
+      member s.top = 
+        if list.IsEmpty then None
+        else Some list.Head 
+      member s.pop  = 
+        if list.IsEmpty then 
+          None
+        else 
+          let head = list.Head
+          list <- list.Tail
+          Some head
+      member s.printf =
+        let rec print list =
+          match list with
+          |[] -> ()
+          |a :: b -> printf "%A " a
+                     print b
+        list <- List.rev list
+        print list
+        printfn ""
+      member s.size =
+        let rec size list count =
+          match list with 
+          |[] -> count
+          |a :: b -> size b (count + 1)
+        size list 0
+      member s.rev = list <- List.rev list             
+  end
+
+let postfix (expr: string) =
+  let mutable tokens = []
+  let mutable lexema = ""
+  for i in 0..expr.Length - 1 do
+    match expr.[i] with
+    |'-' -> if expr.[i-1] = '(' then lexema <- "-"
+            else 
+              if System.Char.IsDigit(expr.[i-1]) then 
+                   tokens <- List.append tokens [lexema]
+                   lexema <- ""
+              tokens <- List.append tokens ["-"]
+    |'(' -> tokens <- List.append tokens ["("]
+    |')' -> tokens <- List.append tokens [lexema]
+            lexema <- ""
+            tokens <- List.append tokens [")"]
+    |' ' -> if System.Char.IsDigit(expr.[i-1]) then
+              tokens <- List.append tokens [lexema]
+              lexema <- ""
+    | _ -> if System.Char.IsDigit(expr.[i]) then lexema <- lexema + expr.[i].ToString()
+           else 
+             if (lexema <> "")&&(System.Char.IsDigit(expr.[i-1])) then 
+                  tokens <- List.append tokens [lexema]
+                  lexema <- ""
+             tokens <- List.append tokens [expr.[i].ToString()]
+                
+  if lexema.Length <> 0 then tokens <- List.append tokens [lexema]
+
+
+  let prior op = 
+      match op with
+      |"^" -> 3
+      |"*" -> 2
+      |"/" -> 2
+      |"%" -> 2
+      |"+" -> 1
+      |"-" -> 1
+      | _  -> 0
+  let op x = (prior x <> 0)
+  
+  let stackOut = new ListStack<string>():> Stack<string>
+  let stackOp = new ListStack<string>():> Stack<string>
+  let rec f t =
+    match t with
+    |[] -> while not (stackOp.isEmpty) do
+             if stackOp.top = Some ")" then
+               while stackOp.top <> Some "(" do 
+                 (stackOut.push (stackOp.top.Value))       
+                 ignore(stackOp.pop)     
+               ignore(stackOp.pop)
+             else
+               stackOut.push (stackOp.top.Value)
+               ignore(stackOp.pop)             
+    |x :: y -> match x with
+               |"(" -> stackOp.push ("(")
+               |")" -> while stackOp.top.Value <> "(" do 
+                         stackOut.push (stackOp.top.Value)
+                         ignore(stackOp.pop)
+                       ignore(stackOp.pop)
+               | _ -> if Char.IsLetter x.[0]  then stackOut.push x
+                      if (x.Length > 1)&&(not (System.Char.IsDigit(x.[0]))) then 
+                        stackOut.push x
+                      elif (x.Length > 0)&&(System.Char.IsDigit(x.[0])) then stackOut.push x
+                      elif op x then 
+                        if stackOp.isEmpty then ignore(stackOp.push(x))
+                        elif (prior x <= (prior stackOp.top.Value))&&(prior x < 3) then 
+                          (stackOut.push (stackOp.top.Value))       
+                          ignore(stackOp.pop)
+                          stackOp.push(x)
+                        elif (prior x = 3) then 
+                          stackOp.push(x)
+                        else stackOp.push(x)
+               f y                                                          
+  f tokens
+  stackOut
+
+
+let divByZero x y =
+  if y = 0 then failwith "Деление на ноль невозможно"
+  else x / y
+
+let stackmachine (expr: string) =
+  let stackOut = new ListStack<string>():> Stack<string>
+  let stackVal = new ListStack<int>():> Stack<int>
+  let stackOut = postfix expr
+  stackOut.rev
+  while not(stackOut.isEmpty) do
+    let token = stackOut.top.Value
+    ignore(stackOut.pop)
+    if (token.Length > 1)&&(not (System.Char.IsDigit(token.[0]))) then stackVal.push (System.Convert.ToInt32(token))
+    elif System.Char.IsDigit(token.[0]) then stackVal.push (System.Convert.ToInt32(token))
+    else 
+      if stackVal.size > 1 then
+        let x = stackVal.top.Value
+        ignore(stackVal.pop)
+        let y = stackVal.top.Value
+        ignore(stackVal.pop)
+        match token with
+            |"^" -> stackVal.push (pown y x)
+            |"*" -> stackVal.push (y * x)
+            |"/" -> stackVal.push (divByZero y x)
+            |"%" -> stackVal.push (y % x)
+            |"+" -> stackVal.push (y + x)
+            |"-" -> stackVal.push (y - x)
+            |_ -> () 
+  stackVal.top.Value
+
+let check ex =
+  try 
+    stackmachine ex
+  with
+  |Failure msg -> printfn "%s" msg; 0
---
+++ Hw08/src/test/test.fs
@@ -0,0 +1,15 @@
+﻿module test
+
+open Calculation 
+open NUnit.Framework
+
+[<TestCase ("3 ^ 1 ^ 2", Result = 3)>]
+[<TestCase ("(3 ^ 1) ^ 2", Result = 9)>]
+[<TestCase ("1 - 2 - 3", Result = -4)>]
+[<TestCase ("1 - (2 - 3)", Result = 2)>]
+[<TestCase ("24 / 12", Result = 2)>]
+[<TestCase ("1+999999", Result = 1000000)>]
+[<TestCase ("21 % 2 + 3 ^ 2 *(1 - 4)", Result = -26)>]
+[<TestCase ("1 / 0", Result = 0)>]
+let ``Тесты на вычисление выражения`` expr =
+  check expr
